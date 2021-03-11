1877db12e1dec57689087867332e33754b8de444
Jacob Pratley <pratleyjacob@gmail.com>
2021-03-11 17:18:02 -0500

    Starter Project

---
+++ GDW II Skinned Mesh/res/shaders/skinned_vertex_shader.glsl
@@ -0,0 +1,41 @@
+#version 410
+
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec3 inNormal;
+layout(location = 3) in vec2 inUV;
+
+layout(location = 0) out vec3 outPos;
+layout(location = 1) out vec3 outColor;
+layout(location = 2) out vec3 outNormal;
+layout(location = 3) out vec2 outUV;
+
+uniform mat4 u_ModelViewProjection;
+uniform mat4 u_View;
+uniform mat4 u_Model;
+uniform mat3 u_NormalMatrix;
+uniform vec3 u_LightPos;
+
+
+void main() {
+
+	vec3 pos = inPosition;
+	
+	gl_Position = u_ModelViewProjection * vec4(pos, 1.0);
+
+
+	// Standard vertex shader business
+	// Pass vertex pos in world space to frag shader
+	outPos = (gl_Position).xyz + (0, 1, 0);
+
+	// Normals
+	outNormal = u_NormalMatrix * inNormal;
+
+	// Pass our UV coords to the fragment shader
+	outUV = inUV;
+
+	///////////
+	outColor = inColor;
+
+}
+
